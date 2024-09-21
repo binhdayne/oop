d42a35a5347aa2aaa626fbfca3131dcbe497e060
binhdayne <23020516@vnu.edu.vn>
2024-09-22 00:51:32 +0700

    add introduction

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -4,10 +4,14 @@ import java.sql.SQLOutput;
 public class Introduction {
 
     public static void main(String[] args) {
-        System.out.print("DUONG THANH BINH\t" + "23020516\t" + "K68-IS\t" + "binhdayne\t" + "duongthanhbinh1411@gmail.com\t");
+        System.out.print("DUONG THANH BINH\t" + "23020516\t" + "K68-IS\t" + "binhdayne\t" + "duongthanhbinh1411@gmail.com\n");
         for (int i = 9; i>0; i-- ) {
             System.out.println(i + " bottles of beer on the wall, " + i +  " bottles of beer.");
             System.out.println("Take one down, pass it around,");
+            if (i==1) {
+                System.out.println(i + " bottle of beer on the wall, " + i +  " bottle of beer.");
+                System.out.println("Take one down, pass it around,");
+            }
         }
         System.out.println("No more bottles of beer on the wall.");
     }
