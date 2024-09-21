2c140e504900d1f99d6fa75d2b85049fee63d7e4
binhdayne <23020516@vnu.edu.vn>
2024-09-22 00:32:02 +0700

    add introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,15 @@
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.print("DUONG THANH BINH\t" + "23020516\t" + "K68-IS\t" + "binhdayne\t" + "duongthanhbinh1411@gmail.com\t");
+        for (int i = 9; i>0; i-- ) {
+            System.out.println(i + " bottles of beer on the wall, " + i +  " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+
+}
